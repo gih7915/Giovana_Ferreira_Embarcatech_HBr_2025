58e735a8b2e5711e097d1f11fc2d649c19b7a89b
Giovana Ferreira Santos <134656592+gih7915@users.noreply.github.com>
2025-05-02 17:26:31 -0300

    Update hal_led.c

--- Projects/Code_Structuring/hal/hal_led.c
+++ Projects/Code_Structuring/hal/hal_led.c
@@ -1,1 +1,13 @@
+#include "led_embutido.h"
 
+bool led_initiated = false;
+
+void hal_led_toggle(void) 
+{
+    if(!led_initiated) 
+    {
+        led_initiated = led_embutido_init();
+    }
+
+    led_embutido_set(!led_embutido_get());
+}
