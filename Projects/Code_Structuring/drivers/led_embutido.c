d6986db2e19dd70399481ed0c703787b98978ed3
Giovana Ferreira Santos <134656592+gih7915@users.noreply.github.com>
2025-05-02 17:25:56 -0300

    Update led_embutido.c

--- Projects/Code_Structuring/drivers/led_embutido.c
+++ Projects/Code_Structuring/drivers/led_embutido.c
@@ -1,1 +1,21 @@
+#include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
 
+bool led_embutido_init(void) 
+{
+    if(cyw43_arch_init()) 
+    {
+        return false;
+    }
+    return true;
+} 
+
+void led_embutido_set(bool estado) 
+{
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, estado);
+}
+
+bool led_embutido_get(void) 
+{
+    return cyw43_arch_gpio_get(CYW43_WL_GPIO_LED_PIN);
+}
