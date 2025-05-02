9cd7090d100503aabe59e791d3afcfe2cccf9957
Giovana Ferreira Santos <134656592+gih7915@users.noreply.github.com>
2025-05-02 17:26:52 -0300

    Update led_embutido.h

--- Projects/Code_Structuring/include/led_embutido.h
+++ Projects/Code_Structuring/include/led_embutido.h
@@ -1,1 +1,11 @@
+#ifndef LED_EMBUTIDO_H
 
+#define LED_EMBUTIDO_H
+
+#include <stdbool.h>
+
+bool led_embutido_init(void);
+void led_embutido_set(bool ligar);
+bool led_embutido_get(void);
+
+#endif
