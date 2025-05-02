1aad088617e649876bca2edccaee36de8c7e5082
Giovana Ferreira Santos <134656592+gih7915@users.noreply.github.com>
2025-05-02 17:25:24 -0300

    Update main.c

--- Projects/Code_Structuring/app/main.c
+++ Projects/Code_Structuring/app/main.c
@@ -1,1 +1,12 @@
+#include "hal_led.h"
+#include "pico/stdlib.h"
 
+int main(void) 
+{
+    while (true) 
+    {
+        hal_led_toggle(); //Led acende
+        sleep_ms(500); //Pausa de 0.5 segundos
+    }
+    return 0;
+}
